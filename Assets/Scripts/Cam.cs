6a7d9571732fa6369332ec71d278415be408e48c
SigaaAE <giancarlojunior@hotmail.com>
2017-01-20 23:34:10 -0300

    Initial

---
+++ Assets/Scripts/Cam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cam : MonoBehaviour {
+	public Color color1 = Color.red;
+	public Color color2 = Color.cyan;
+
+	Camera camera;
+
+	void Start() {
+		camera = GetComponent<Camera>();
+		camera.clearFlags = CameraClearFlags.SolidColor;
+	}
+
+	void Update() {
+		int time = System.DateTime.Now.Millisecond + System.DateTime.Now.Second * 1000 + System.DateTime.Now.Minute * 60000 + System.DateTime.Now.Hour * 3600000;
+		float t = Mathf.PingPong(Time.time, time) / time;
+		camera.backgroundColor = Color.Lerp(color1, color2, t);
+	}
+}
---
+++ Assets/Scripts/PlataformGenerator.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlataformGenerator: MonoBehaviour {
+
+	public Transform StartPlataformGenerator;
+	public float speed = 50f;
+	GameObject NewPlataform;
+	GameObject Player;
+	System.Random random = new System.Random();
+
+	int ChildCount;
+	Vector3 EndPosition;
+	Vector3 EndPosAdjust = new Vector3(1f,0,0);
+
+	void Start () 
+	{
+		NewPlataform = Instantiate(Resources.Load("Prefabs/plataforms/Plat1") as GameObject,StartPlataformGenerator.position,Quaternion.identity) as GameObject;
+		Player = Instantiate(Resources.Load("Prefabs/characters/Player1") as GameObject,StartPlataformGenerator.position + new Vector3(0, 3f, 0),Quaternion.identity) as GameObject;
+	}
+	void Update()
+	{
+			try{
+				ChildCount = NewPlataform.transform.childCount;
+				for (int i=0; i<ChildCount; i++)
+				{
+					if (NewPlataform.transform.GetChild(i).name == "End")
+					{
+						EndPosition = NewPlataform.transform.GetChild(i).position + EndPosAdjust;
+						break;
+					}
+				}
+				if (EndPosition.x < 100f)
+				{
+					NewPlataform = Instantiate(newPlataform(),EndPosition,Quaternion.identity) as GameObject;
+				}
+
+				foreach (var plataforma in GameObject.FindGameObjectsWithTag("Plataforma"))
+				{
+					plataforma.transform.Translate(-speed*Time.deltaTime,0,0);
+
+					if ( plataforma.transform.position.x < -100f )
+					{
+						Destroy(plataforma);
+					}
+				} 
+			}
+			catch(MissingReferenceException){}
+	}
+
+	GameObject newPlataform()
+	{
+		return Resources.Load("Prefabs/plataforms/Plat"+random.Next(1,4).ToString()) as GameObject;
+	}
+
+
+}
